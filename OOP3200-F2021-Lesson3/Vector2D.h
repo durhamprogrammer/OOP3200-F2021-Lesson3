b22859eed030b12eed4085c00947b0abec2b5bb1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-09-20 09:17:19 -0400

    Add project files.

---
+++ OOP3200-F2021-Lesson3/Vector2D.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef __VECTOR_2D__
+#define __VECTOR_2D__
+
+class Vector2D
+{
+public:
+	// CONSTRUCTOR(S)
+
+	// DESTRUCTOR
+
+	// GETTERS (Accessors)
+
+	// SETTERS (Mutators)
+
+	// PUBLIC METHODS (Public Class Functions)
+
+private:
+	// INSTANCE VARIABLES (Class Member Variables)
+
+	// PRIVATE METHODS (Private Class Functions)
+};
+
+#endif /* defined (__VECTOR_2D__) */
---
+++ OOP3200-F2021-Lesson3/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: Tom Tsiliopoulos
+ * StudentID: 100100100
+ *
+ * Date: September 20, 2021
+ * Description: Demo Project for OOP3200 - F2021 - Week 3
+ * 
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
