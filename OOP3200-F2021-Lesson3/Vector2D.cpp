b5e63015562c3424b099507d9810b3bc196dfbf6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-09-20 09:42:34 -0400

    Added First Draft of Vector2D Class

--- OOP3200-F2021-Lesson3/Vector2D.cpp
+++ OOP3200-F2021-Lesson3/Vector2D.cpp
@@ -1,1 +1,42 @@
 #include "Vector2D.h"
+
+Vector2D::Vector2D()
+{
+	Set();
+}
+
+Vector2D::~Vector2D()
+= default;
+
+float Vector2D::GetX() const
+{
+	return m_x;
+}
+
+float Vector2D::GetY() const
+{
+	return m_y;
+}
+
+void Vector2D::SetX(const float x = 0.0f)
+{
+	m_x = x;
+}
+
+void Vector2D::SetY(const float y = 0.0f)
+{
+	m_y = y;
+}
+
+void Vector2D::Set(const float x = 0.0f, const float y = 0.0f)
+{
+	SetX(x);
+	SetY(y);
+}
+
+std::string Vector2D::ToString() const
+{
+	std::string output_string;
+	output_string+= "(" + std::to_string(GetX()) + ", " + std::to_string(GetY()) + ")\n";
+	return output_string;
+}
--- OOP3200-F2021-Lesson3/Vector2D.h
+++ OOP3200-F2021-Lesson3/Vector2D.h
@@ -2,21 +2,35 @@
 #ifndef __VECTOR_2D__
 #define __VECTOR_2D__
 
+#include <string>
+
 class Vector2D
 {
 public:
 	// CONSTRUCTOR(S)
+	Vector2D();
 
 	// DESTRUCTOR
+	~Vector2D();
 
 	// GETTERS (Accessors)
+	float GetX() const;
+	float GetY() const;
 
 	// SETTERS (Mutators)
+	void SetX(float x);
+	void SetY(float y);
+	void Set(float x, float y);
 
 	// PUBLIC METHODS (Public Class Functions)
+	std::string ToString() const;
+
+	// PUBLIC STATIC METHODS (Public Class Functions that are Static)
 
 private:
 	// INSTANCE VARIABLES (Class Member Variables)
+	float m_x;
+	float m_y;
 
 	// PRIVATE METHODS (Private Class Functions)
 };
